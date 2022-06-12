02c9fef13820a1bb4f9483c16606b0bd6ba9831d
tofu0924 <in_no@naver.com>
2022-06-12 23:47:26 +0900

    square

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+      <div>
+        <Square className="square"/>
+        {' '}
+        <Square className="square"/>
+      </div>
+    );
+  }
+}
+
+class Square extends React.Component{
+  constructor(props){
+  super(props);
+  console.log(props)
+}
+render(){
+  const className = this.props.className
+  return(
+      <div className={className}>
+     </div>
+  )
+}  
 }
 
 export default App;
